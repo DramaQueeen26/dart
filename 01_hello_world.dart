07a0f8bb6c5110c6bf1d0ce3676c64bc500da4e4
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-03-02 11:40:36 -0400

    Introducción a Dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,8 @@
+void main(){
+  //var myName = 'Mariangel';
+  String myName = 'Mariangel';
+  final myName2 = 'Mariangel'; //No se le puede cambiar el valor después de asignarlo (similar a constante)
+  
+  print('Hola ${myName.toLowerCase()}');
+  
+}
